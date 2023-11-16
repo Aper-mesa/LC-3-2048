1bc48d163c42904c0694bbf9a3320f58fa2b2c44
Reversal Destiny Studio <133374491+AnodHuang@users.noreply.github.com>
2023-11-16 14:11:58 +0800

    Update If_Else_KunYangHuang.asm
    If - else 结构的实现

--- LC3_Codes/If_Else_KunYangHuang.asm
+++ LC3_Codes/If_Else_KunYangHuang.asm
@@ -8,7 +8,7 @@ BRnp ELSE   ; Otherwise -> To ELSE symbol
 RESULT:  .FILL x0000    ; Init the value of RESULT (value 0)
 NUMA:  .FILL x0001  ; Init value for if statement
 NUMB:  .FILL x0002  ; Init value for else statement
-CONDITION:  .FILL x0001
+CONDITION:  .FILL x0010
 IF: LD R0, NUMA ; Set the value of R0 to 0
     BR DONE  ; To the end of the program
 ELSE: LD R0, NUMB ; Set the value of R0 to 1
