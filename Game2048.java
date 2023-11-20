a5b78864bfac3cf3975dba537fc0b750d208fc33
Nuojunxi Zhang <59161562+Steven-ZN@users.noreply.github.com>
2023-11-20 17:51:21 +0800

    Update Game2048.java

--- Game2048.java
+++ Game2048.java
@@ -84,55 +84,66 @@ public class Game2048 {
     }
 
     //移动
-    private static void move(int direction) {
+private static void move(int direction) {
+        // 一种新思路是 用一个dowhile嵌套减少无效遍历 如果一个方向不可能继续移动就停止循环
+        final int size = 4;
+        boolean tileMoved;
         if (direction == UP) {
-            for (int k = 0; k < 3; k++) {
-                for (int j = 0; j < 4; j++) {
-                    for (int i = 0; i < 3; i++) {
+            do {
+                tileMoved = false;
+                for (int j = 0; j < size; j++) {
+                    for (int i = 0; i < size - 1; i++) {
                         if (board[i][j] == 0 && board[i + 1][j] != 0) {
-                            changed = true;
                             board[i][j] = board[i + 1][j];
                             board[i + 1][j] = 0;
+                            tileMoved = true;
+                            changed = true;
                         }
                     }
                 }
-            }
+            } while (tileMoved);
         } else if (direction == DOWN) {
-            for (int k = 0; k < 3; k++) {
-                for (int j = 0; j < 4; j++) {
-                    for (int i = 3; i > 0; i--) {
+            do {
+                tileMoved = false;
+                for (int j = 0; j < size; j++) {
+                    for (int i = size - 1; i > 0; i--) {
                         if (board[i][j] == 0 && board[i - 1][j] != 0) {
-                            changed = true;
                             board[i][j] = board[i - 1][j];
                             board[i - 1][j] = 0;
+                            tileMoved = true;
+                            changed = true;
                         }
                     }
                 }
-            }
+            } while (tileMoved);
         } else if (direction == LEFT) {
-            for (int k = 0; k < 3; k++) {
-                for (int i = 0; i < 4; i++) {
-                    for (int j = 0; j < 3; j++) {
+            do {
+                tileMoved = false;
+                for (int i = 0; i < size; i++) {
+                    for (int j = 0; j < size - 1; j++) {
                         if (board[i][j] == 0 && board[i][j + 1] != 0) {
-                            changed = true;
                             board[i][j] = board[i][j + 1];
                             board[i][j + 1] = 0;
+                            tileMoved = true;
+                            changed = true;
                         }
                     }
                 }
-            }
+            } while (tileMoved);
         } else if (direction == RIGHT) {
-            for (int k = 0; k < 3; k++) {
-                for (int i = 0; i < 4; i++) {
-                    for (int j = 3; j > 0; j--) {
+            do {
+                tileMoved = false;
+                for (int i = 0; i < size; i++) {
+                    for (int j = size - 1; j > 0; j--) {
                         if (board[i][j] == 0 && board[i][j - 1] != 0) {
-                            changed = true;
                             board[i][j] = board[i][j - 1];
                             board[i][j - 1] = 0;
+                            tileMoved = true;
+                            changed = true;
                         }
                     }
                 }
-            }
+            } while (tileMoved);
         }
     }
 
