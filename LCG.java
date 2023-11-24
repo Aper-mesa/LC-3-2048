71ed4235a154b831f202fcaeab6f058ec4da6e66
邹上豪 <zoush@kean.edu>
2023-11-24 13:25:55 +0800

    LCG实现

--- LCG.java
+++ LCG.java
@@ -1,14 +1,14 @@
 public class LCG {
     public static void main(String[] args) {
-        int x1 = 1998;
         int a = 11;
         int c = 19;
-        int m = 32749;
+        int m = 10007;
         int before = -1;
-        for (int i = 0; i < 100; i++) {
-            if (before == -1) before = 1998;
+        for (int i = 0; i < 50; i++) {
+            if (before == -1) before = 1997;
             int next = (a * before + c) % m;
             int result = next % 4;
+            System.out.println(next);
             System.out.println(result);
             before = next;
         }
